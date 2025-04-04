854499ff9efece23b2c8d7fc1c2412910492c34d
Vadim Droshenkov <vadimdroshenkov@gmail.com>
2025-04-05 01:36:05 +0300

    Добавлен метод в DAO и Service Добавлена связь OneToOne Добавлен в AppConfig Car.class Создана модель Car Добавлены тесты в Main

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -1,6 +1,7 @@
 package hiber.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "users")
@@ -9,6 +10,10 @@ public class User {
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
+
+   @OneToOne(cascade = CascadeType.ALL)
+   @JoinColumn(name = "car_id", unique = true)
+   private Car car;
 
    @Column(name = "name")
    private String firstName;
@@ -25,6 +30,13 @@ public class User {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
+   }
+
+   public User(String firstName, String lastName, String email, Car car) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.email = email;
+      this.car = car;
    }
 
    public Long getId() {
@@ -57,5 +69,39 @@ public class User {
 
    public void setEmail(String email) {
       this.email = email;
+   }
+
+   public Car getCar() {
+      return car;
+   }
+
+   public void setCar(Car car) {
+      this.car = car;
+   }
+
+   @Override
+   public String toString() {
+      return "User id = " + id +
+              "\nfirstName = " + firstName +
+              "\nlastName = " + lastName +
+              "\nemail = " + email +
+              "\ncar = " + (car == null ? "none" : car);
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      User user = (User) o;
+      return id.equals(user.id)
+              && car.equals(user.car) &&
+              firstName.equals(user.firstName) &&
+              lastName.equals(user.lastName) &&
+              email.equals(user.email);
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(id, car, firstName, lastName, email);
    }
 }
